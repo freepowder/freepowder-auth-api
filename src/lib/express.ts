7aa2147e9ffc7cb61bf387feb3f374dde29de200
afernandez <afernandez@bspot.com>
2023-05-11 12:49:23 +0100

    CORS

--- src/lib/express.ts
+++ src/lib/express.ts
@@ -48,6 +48,26 @@ class ExpressConfig {
 	}
 
 	private initJwt(): void {
+
+		this.app.use((req, res, next) => {
+
+			res.setHeader('Access-Control-Allow-Credentials', 'true');
+			res.setHeader('Access-Control-Allow-Origin', '*');
+			// another common pattern
+			// res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+			res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+			res.setHeader(
+				'Access-Control-Allow-Headers',
+				'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+			);
+			if (req.method === 'OPTIONS') {
+				res.status(200).end();
+				return;
+			}
+			return next(req);
+		});
+
+
 		const whitelist = ['https://wierzbianski.freepowder.io', 'https://*.freepowder.io'];
 		const corsOptions = {
 			origin: function(origin, callback) {
